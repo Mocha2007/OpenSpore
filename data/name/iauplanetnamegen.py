e83c6466f2c0f17978e484a5c112463a60703dbe
Mocha <mocha2007andcoco@gmail.com>
2018-10-14 21:12:56 -0400

    namegen works above 26 bodies

--- data/name/iauplanetnamegen.py
+++ data/name/iauplanetnamegen.py
@@ -4,4 +4,6 @@ from constants import alphabet
 
 
 def main(starname, position) -> str:
+	if position > 25:
+		return starname + ' ' + alphabet[position // len(alphabet)] + alphabet[position % len(alphabet)]
 	return starname + ' ' + alphabet[position % len(alphabet)]
