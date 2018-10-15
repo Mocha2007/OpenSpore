70834874d53a13d15be65da9e1d42e74fcc37e56
Mocha <mocha2007andcoco@gmail.com>
2018-10-14 21:32:40 -0400

    better planets (again)

--- data/mapmode/planets.py
+++ data/mapmode/planets.py
@@ -5,7 +5,7 @@ from system import System
 
 
 def main(system: System) -> Color:
-	return Color(128, min(int(len(system.bodies)**2*3), 255), 128)
+	return Color(128, min(len(system.bodies)*255//9, 255), 128)
 
 
 planet = main
