13ae08c3e6824e86820c3dfca4504c5d75910a72
Mocha <mocha2007andcoco@gmail.com>
2018-10-22 18:24:43 -0400

    maxmin mode

---
+++ data/mapmode/maxmin.py
@@ -0,0 +1,32 @@
+import sys
+from math import log
+sys.path.append('./data')
+from color import Color
+from system import System, Planet
+from constants import m_browndwarf, m_rock
+
+
+def m2u(mass: float) -> float:
+	mrange = log(m_browndwarf) - log(m_rock)
+	return (log(mass)-log(m_rock))/mrange
+
+
+def m2c(minimum: float, maximum: float) -> Color:
+	r = 255 - round(m2u(minimum) * 255)
+	b = round(m2u(maximum) * 255)
+	return Color(r, 128, b)
+
+
+def main(system: System) -> Color:
+	maximum = 0
+	minimum = m_browndwarf
+	for _, p in system.bodies:
+		if p.mass > maximum:
+			maximum = p.mass
+		if p.mass < minimum:
+			minimum = p.mass
+	return m2c(minimum, maximum)
+
+
+def planet(p: Planet) -> Color:
+	return m2c(p.mass, p.mass)
