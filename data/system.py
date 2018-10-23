1085fe9eb6e374f933977b4e0589cf399b644ae4
Mocha <mocha2007andcoco@gmail.com>
2018-10-23 17:34:39 -0400

    moonfix

--- data/system.py
+++ data/system.py
@@ -84,7 +84,7 @@ class Planet: # no type annotation since function can't be annotated
 		soi_ = soi(self)
 		if soi_moon < soi_:
 			maxmoons = round(3.3e-5 * soi_**.5) # not perfect, but certainly more realistic than before!
-			for i in range(1, randint(0, maxmoons)):
+			for i in range(0, randint(1, maxmoons)):
 				contents.append((i, Moon(self, system, lambda: moonnamegen(self.name, i), resourcegen)))
 		self.bodies = contents
 		data = {
