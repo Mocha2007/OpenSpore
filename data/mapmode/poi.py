413d3cef3cddb167b9dfb983b8ec21b2b21dc874
Mocha <mocha2007andcoco@gmail.com>
2018-10-23 18:01:59 -0400

    added toxic world and optimized

--- data/mapmode/poi.py
+++ data/mapmode/poi.py
@@ -3,6 +3,9 @@ sys.path.append('./data')
 from color import Color
 from system import System, Planet
 from constants import grey, atmchems, m_earth, m_gg, m_ig, m_j, ishab, t_earth
+from toxic import istox
+sys.path.append('./data/surface')
+from continental import gettype, typecolor
 
 # WARNING this module is super laggy; I get 14 fps from this!!!
 
@@ -14,31 +17,48 @@ poicolors = {
 	'Mesoplanet': Color(255, 192, 192),
 	'Mini-Neptune': Color(128, 192, 255),
 	'Superhabitable': Color(255, 128, 0),
-	'Super-Jupiter': Color(255, 224, 192)
+	'Super-Jupiter': Color(255, 224, 192),
+	'Toxic': typecolor[.1][0].blend(typecolor[.1][1])
 }
 
 
 def poi(p: Planet) -> str:
 	"""See if planet contains an oddity!"""
-	# Superhabitable https://en.wikipedia.org/wiki/Superhabitable_planet
-	# "... superhabitable worlds would likely be larger, warmer,
-	# and older than Earth, and orbiting K-type main-sequence stars."
-	if p.mass > m_earth and p.temp > t_earth and ishab(p):
-		return 'Superhabitable'
-	# Hot Jupiter/Neptune
-	if p.mass > m_gg and p.temp > t_earth:
-		if p.mass > m_ig:
-			return 'Hot Jupiter'
-		return 'Hot Neptune'
+	# Gas giants
+	if m_gg < p.mass:
+		# Hot Jupiter/Neptune
+		if t_earth < p.temp:
+			if p.mass > m_ig:
+				return 'Hot Jupiter'
+			return 'Hot Neptune'
+		# Mini-Neptune
+		if p.mass < 8.681e25:
+			return 'Mini-Neptune'
+		# Super-Jupiter
+		if m_j < p.mass:
+			return 'Super-Jupiter'
+		# the following are currently impossible and thus temporarily blocked for performance
+		return ''
+		# Helium Giant
+		atm = atmchems(p)
+		if 'He' in atm:
+			if max(p.atmosphere.items(), key=lambda x: x[1]) == 'He':
+				return 'Helium Giant'
+		return ''
+	# otherwise, MUST be terrestrial
+	# "Habitables"
+	if ishab(p):
+		# Superhabitable https://en.wikipedia.org/wiki/Superhabitable_planet
+		# "... superhabitable worlds would likely be larger, warmer,
+		# and older than Earth, and orbiting K-type main-sequence stars."
+		if p.mass > m_earth and p.temp > t_earth:
+			return 'Superhabitable'
+		# Toxics
+		if istox(p):
+			return 'Toxic'
 	# Mesoplanet
 	if p.mass < 3.3011e23:
 		return 'Mesoplanet'
-	# Mini-Neptune
-	if m_gg < p.mass < 8.681e25:
-		return 'Mini-Neptune'
-	# Super-Jupiter
-	if m_j < p.mass:
-		return 'Super-Jupiter'
 	# the following are currently impossible and thus temporarily blocked for performance
 	return ''
 	# Carbon Planet
@@ -47,11 +67,6 @@ def poi(p: Planet) -> str:
 		oxygen = sum(map(lambda x: x[1], filter(lambda x: 'O' in x[0], p.atmosphere.items())))
 		if carbon > oxygen:
 				return 'Carbon Planet'
-	# Helium Giant
-	atm = atmchems(p)
-	if 'He' in atm:
-		if max(p.atmosphere.items(), key=lambda x: x[1]) == 'He':
-			return 'Helium Giant'
 	return ''
 
 
