0acdcce2f4f1c1cf5eb1927b843cf11d0a3e5bd5
Mocha <mocha2007andcoco@gmail.com>
2018-11-26 09:52:34 -0500

    more names

--- data/name/civnamegen.py
+++ data/name/civnamegen.py
@@ -16,18 +16,29 @@ def bothname(n: str):
 	return forename(n) if random() < .5 else hindname(n)
 
 
+def neithername(n: str):
+	return lambda name: name
+
+
+def aroundname(n: (str, str)):
+	return lambda name: n(0) + name + n(1)
+
+
 # type, string
 types = (
+	(neithername, ''),
 	(bothname, 'Commonwealth'),
 	(bothname, 'Confederacy'),
 	(bothname, 'Confederation'),
 	(bothname, 'Empire'),
 	(bothname, 'Federation'),
+	(forename, 'Grand Duchy'),
 	(bothname, 'Kingdom'),
 	(bothname, 'Principality'),
 	(bothname, 'Republic'),
 	(bothname, 'State'),
 	(bothname, 'Technate'),
+	(aroundname, ('United', 'States')),
 )
 
 
