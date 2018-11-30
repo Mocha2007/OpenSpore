2760c067d02b76f39e729017c9d2d25656007345
Mocha <mocha2007andcoco@gmail.com>
2018-11-30 13:30:05 -0500

    source sort

--- data/name/allusion.py
+++ data/name/allusion.py
@@ -2,12 +2,19 @@ from random import choice
 from json import load
 
 names = load(open('data/name/allusion.json', encoding="utf-8"))
-universal = list(filter(lambda x: x[0] != '_', names['Any']))
+sets = {}
+for _, name_list in names.items():
+	for name, types in name_list.items():
+		for char in types:
+			if char in sets:
+				sets[char].add(name)
+			else:
+				sets[char] = {name}
 
 
 def main(star) -> str:
-	sources = universal[:]
-	if star.type in names:
-		sources += names[star.type]
-	sources = list(set(sources))
-	return choice(sources)
+	sources = set(sets['.']) # gotta make it a new set
+	if star.type in sets:
+		sources = sources.union(sets[star.type])
+	print(sources)
+	return choice(list(sources))
