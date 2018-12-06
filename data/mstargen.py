cb91063d393267e7f75bb26f35b30faeaf359a4a
Mocha <mocha2007andcoco@gmail.com>
2018-12-06 12:47:07 -0500

    better brown dwarf distro

--- data/mstargen.py
+++ data/mstargen.py
@@ -4,7 +4,11 @@ from random import random
 def main() -> float:
 	# https://www.nasa.gov/vision/universe/starsgalaxies/brown_dwarf_detectives.html
 	# "There are probably around the same number of brown dwarfs as stars within the immediate solar neighborhood."
-	if random() > .5:
+	# ie. 50%
+	# https://www.centauri-dreams.org/2017/07/07/how-many-brown-dwarfs-in-the-milky-way/
+	# "For every ten stars there are five brown dwarfs"
+	# ie. 33%
+	if .4 < random(): # the float is the chance of becoming a brown dwarf (kinda)
 		# star will fall approximately within normal distribution
 		n = 13
 	else:
